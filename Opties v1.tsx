c355493b506b7039c53c8c7aaca833d5c0c8b1ee
Marijn-Snoeren <m.snoeren@student.fontys.nl>
2024-12-17 21:54:54 +0100

    Update Opties v1.tsx

--- Opties v1.tsx
+++ Opties v1.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import { useQuery, gql } from "@apollo/client";
-import AOS from "aos";
-import "aos/dist/aos.css";
 
 const BLOCK_A_QUERY = gql`
   query BlockAQuery($slug: String!) {
@@ -34,16 +32,6 @@ const BlockG: React.FC = () => {
     variables: { slug: postSlug },
   });
 
-  useEffect(() => {
-    // Initialiseer AOS
-    AOS.init();
-  }, []);
-
-  useEffect(() => {
-    // Zorg ervoor dat AOS opnieuw wordt geladen wanneer de component opnieuw wordt weergegeven
-    AOS.refresh();
-  }, [data]);
-
   if (loading) return <p className="text-center">Loading...</p>;
   if (error) return <p className="text-red-500">Error: {error.message}</p>;
 
@@ -60,118 +48,37 @@ const BlockG: React.FC = () => {
   } = data.postBy.content;
 
   return (
-    <div className="pt-16 pb-16 bg-lime px-[20px] md:px-[120px]">
-      <div className="grid grid-cols-12 gap-[20px]">
+    <div className="pt-16 pb-16 bg-lime px-4 md:px-32">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {/* Left Column */}
-        <div
-          className="col-span-12 md:col-span-4 flex flex-col items-start text-left"
-          data-aos="fade-up"
-        >
+        <div className="flex flex-col items-start text-left">
           <h2 className="font-subtext text-subtext text-darkgreen leading-tight mb-2">
             {subtext1}
           </h2>
           <p className="font-body text-body text-darkgreen mb-4">{body1}</p>
-          <div className="mb-4">
-            <div className="flex items-center">
-              <span className="mr-2">✔️</span>
-              <span className="font-body text-body text-darkgreen">
-                Protect your data
-              </span>
-            </div>
-            <div className="flex items-center">
-              <span className="mr-2">✔️</span>
-              <span className="font-body text-body text-darkgreen">
-                Datasharing
-              </span>
-            </div>
-            <div className="flex items-center">
-              <span className="mr-2 opacity-0">✔️</span>
-              <span className="font-body text-body text-darkgreen/50">
-                Connecting
-              </span>
-            </div>
-          </div>
         </div>
 
         {/* Middle Column */}
-        <div
-          className="col-span-12 md:col-span-4 flex flex-col items-start text-left"
-          data-aos="fade-up"
-        >
+        <div className="flex flex-col items-start text-left">
           <h2 className="font-subtext text-subtext text-darkgreen leading-tight mb-2">
             {subtext2}
           </h2>
           <p className="font-body text-body text-darkgreen mb-4">{body2}</p>
-          <div className="mb-4">
-            <div className="flex items-center">
-              <span className="mr-2">✔️</span>
-              <span className="font-body text-body text-darkgreen">
-                Visualize
-              </span>
-            </div>
-            <div className="flex items-center">
-              <span className="mr-2">✔️</span>
-              <span className="font-body text-body text-darkgreen">
-                Dashboards
-              </span>
-            </div>
-            <div className="flex items-center">
-              <span className="mr-2">✔️</span>
-              <span className="font-body text-body text-darkgreen">
-                Statistics
-              </span>
-            </div>
-            <div className="flex items-center">
-              <span className="mr-2 opacity-0">✔️</span>
-              <span className="font-body text-body text-darkgreen/50">
-                Marketing
-              </span>
-            </div>
-            <div className="flex items-center">
-              <span className="mr-2 opacity-0">✔️</span>
-              <span className="font-body text-body text-darkgreen/50">
-                Personal Assistance
-              </span>
-            </div>
-          </div>
         </div>
 
         {/* Right Column */}
-        <div
-          className="col-span-12 md:col-span-4 flex flex-col items-start text-left"
-          data-aos="fade-up"
-        >
+        <div className="flex flex-col items-start text-left">
           <h2 className="font-subtext text-subtext text-darkgreen leading-tight mb-2">
             {subtext3}
           </h2>
           <p className="font-body text-body text-darkgreen mb-4">{body3}</p>
-          <div className="mb-4">
-            <div className="flex items-center">
-              <span className="mr-2">✔️</span>
-              <span className="font-body text-body text-darkgreen">
-                Data Analytics
-              </span>
-            </div>
-            <div className="flex items-center">
-              <span className="mr-2">✔️</span>
-              <span className="font-body text-body text-darkgreen">
-                Integrations
-              </span>
-            </div>
-            <div className="flex items-center">
-              <span className="mr-2">✔️</span>
-              <span className="font-body text-body text-darkgreen">
-                Real-time Updates
-              </span>
-            </div>
-            <div className="flex items-center">
-              <span className="mr-2 opacity-0">✔️</span>
-              <span className="font-body text-body text-darkgreen/50">
-                Visualizations
-              </span>
-            </div>
-          </div>
         </div>
+      </div>
+
+      <div className="mt-4 flex justify-center">
+        <button className="font-button bg-green text-darkgreen px-6 h-[40px] rounded-full">
+          {button}
+        </button>
       </div>
     </div>
   );
